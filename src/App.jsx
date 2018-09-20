f3d1908a626e76ff9455272ca4f5b0b6b0e0fdf8
Kento75 <kento2github@gmail.com>
2018-09-20 21:29:03 +0900

    Add article

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Article from "./components/Article/Article";
+import CircleFrame from "./components/ImageFrame/CircleFrame";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <Nav />
+        <CircleFrame imagePath={"./images/favicon.ico"} altName={"kento75"} flameSize={12} />
         <Article />
       </div>
     );
--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import classes from "./Article.css";
 
 const Article = () => {
   return (
-    <article>
-      <h2>ここにタイトル</h2>
-      <p>ここにテキスト</p>
-    </article>
+    <div className={classes.articleContainer}>
+      <div className={classes.articleItem}>
+        <a href="#">
+          <h2>ここに画像</h2>
+          <p>GitHub</p>
+        </a>
+      </div>
+      <div className={classes.articleItem}>
+        <a href="#">
+          <h2>ここに画像</h2>
+          <p>Qiita</p>
+        </a>
+      </div>
+      <div className={classes.articleItem}>
+        <a href="#">
+          <h2>ここに画像</h2>
+          <p>npm</p>
+        </a>
+      </div>
+      <div className={classes.articleItem}>
+        <a href="#">
+          <h2>ここに画像</h2>
+          <p>Twitter</p>
+        </a>
+      </div>
+      <div className={classes.articleItem}>
+        <a href="#">
+          <h2>ここに画像</h2>
+          <p>LinkdIn</p>
+        </a>
+      </div>
+    </div>
   );
 };
 
