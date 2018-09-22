b1b2b7b17d1c4ecc34d31dcb8bd0af0377f874b7
Kento75 <kento2github@gmail.com>
2018-09-22 16:04:55 +0900

    Fix update footer.jsx

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Reactを使用した簡単な自己紹介ページ
 
 [![Build Status](https://travis-ci.org/Kento75/re-portfolio.svg?branch=master)](https://travis-ci.org/Kento75/re-portfolio)
 
-### セットアップ & スタート
+### Quick Start
 
 ```
 $ npm install
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -16,23 +16,23 @@ class FooterComponent extends Component {
     return (
       <div className="footer">
         <div className="link-list">
-          <a target="_blank" href="https://github.com/Kento75">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Kento75">
             Github
           </a>
            /
-          <a target="_blank" href="https://qiita.com/Kento75">
+          <a target="_blank" rel="noopener noreferrer" href="https://qiita.com/Kento75">
             Qiita
           </a>
            /
-          <a target="_blank" href="https://www.npmjs.com/~kento75">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.npmjs.com/~kento75">
             npm
           </a>
           /
-          <a target="_blank" href="https://twitter.com/Kento751">
+          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Kento751">
             Twitter
           </a>
            /
-          <a target="_blank" href="https://www.linkedin.com/in/kento75-4326a316a/">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kento75-4326a316a/">
             LinkedIn
           </a>
         </div>
